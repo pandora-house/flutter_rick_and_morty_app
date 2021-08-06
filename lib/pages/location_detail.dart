4583a6a2696838f9c6e7ebe5f1e5832fd222f987
Mitryaev Dmitrii <pandora-house@mail.ru>
2021-08-06 21:45:17 +0300

    buffering vars

--- lib/pages/location_detail.dart
+++ lib/pages/location_detail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rick_and_morty_app/data/models/character.dart';
+import 'package:flutter_rick_and_morty_app/data/models/locations.dart';
 import '../widgets/resident_item.dart';
 
 import '../data/bloc/rick_and_morty_bloc.dart';
@@ -13,59 +15,77 @@ class LocationDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var location;
+    var residents;
     return Scaffold(
       appBar: AppBar(
         title: Text(name),
       ),
-      body: Center(
-        child: BlocBuilder<RickAndMortyBloc, RickAndMortyState>(
-          builder: (context, state) {
-            if (state is LocationIsLoading) {
-              return CircularProgressIndicator();
-            } else if (state is LocationFetched) {
-              var item = state.item;
-              var _list = state.residents;
-              return Scrollbar(
-                child: SingleChildScrollView(
+      body: BlocBuilder<RickAndMortyBloc, RickAndMortyState>(
+        builder: (context, state) {
+          if (state is LocationIsLoading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is LocationFetched) {
+            var item = state.item;
+            location = item;
+            var _list = state.residents;
+            residents = _list;
+            return _LocationsView(item: item, list: _list);
+          }
+          return location != null
+              ? _LocationsView(item: location, list: residents)
+              : Center(child: Text('smt went wrong'));
+        },
+      ),
+    );
+  }
+}
+
+class _LocationsView extends StatelessWidget {
+  _LocationsView({Key? key, required this.item, required this.list})
+      : super(key: key);
+  final Locations item;
+  final List<Character> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      SizedBox(
-                        width: double.infinity,
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                              Text('Type: ${item.type}'),
-                              Text('Dimension: ${item.dimension}'),
-                            ],),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 0),
-                        child: Text('Residents'),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: _list.length,
-                          itemBuilder: (context, int index) =>
-                              ResidentItemWidget(item: _list[index]),
-                        ),
-                      ),
+                      Text('Type: ${item.type}'),
+                      Text('Dimension: ${item.dimension}'),
                     ],
                   ),
                 ),
-              );
-            }
-            return Text('smt went wrong');
-          },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 0),
+              child: Text('Residents'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: list.length,
+                itemBuilder: (context, int index) =>
+                    ResidentItemWidget(item: list[index]),
+              ),
+            ),
+          ],
         ),
       ),
     );
