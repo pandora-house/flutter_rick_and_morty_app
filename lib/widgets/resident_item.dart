e78bfa6a43954b935df2c608471b45606465fdb0
Mitryaev Dmitrii <pandora-house@mail.ru>
2021-08-06 21:39:21 +0300

    add navigation

--- lib/widgets/resident_item.dart
+++ lib/widgets/resident_item.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../data/bloc/rick_and_morty_bloc.dart';
 import '../data/models/character.dart';
 import '../pages/personage_detail.dart';
 
