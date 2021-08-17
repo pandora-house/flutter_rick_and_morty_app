0a62eef5e34b18b877a34290149cac1332cd5258
Mitryaev Dmitrii <pandora-house@mail.ru>
2021-08-17 10:50:18 +0300

    add new widget

---
+++ lib/widgets/preloading_indicator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PreloadingIndicator extends StatelessWidget {
+  const PreloadingIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child:
+          SizedBox(height: 24, width: 24, child: CircularProgressIndicator()),
+    );
+  }
+}
