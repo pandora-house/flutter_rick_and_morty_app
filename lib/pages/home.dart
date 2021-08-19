df70da4d11cbfcc119727abeae9723d3a1f95afb
Mitryaev Dmitrii <pandora-house@mail.ru>
2021-08-19 17:26:00 +0300

    optimize bottom bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,62 +1,53 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../data/nav_bar_notifiers.dart';
 import 'characters.dart';
 import 'episodes.dart';
 import 'locations.dart';
 
-class HomePage extends StatelessWidget {
-  final _pageController = PageController();
-  final _screens = <Widget>[CharactersPage(), EpisodesPage(), LocationsPage()];
-  final _navBarNotifiers = NavBarNotifier();
-
+class HomePage extends StatefulWidget {
   static const routeName = '/home';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _screens = <Widget>[CharactersPage(), EpisodesPage(), LocationsPage()];
+
+  int _screenIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        onPageChanged: (value) {},
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: _screens.length,
-        itemBuilder: (BuildContext context, int index) {
-          return _screens[index];
-        },
-      ),
-      bottomNavigationBar: ValueListenableBuilder<int>(
-        valueListenable: _navBarNotifiers.index,
-        builder: (context, value, _) {
-          return BottomNavigationBar(
-            items: [
-              BottomNavigationBarItem(
-                label: 'characters',
-                icon: Icon(
-                  Icons.library_books_rounded,
-                ),
+        body: _screens[_screenIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            const BottomNavigationBarItem(
+              label: 'characters',
+              icon: Icon(
+                Icons.library_books_rounded,
               ),
-              BottomNavigationBarItem(
-                label: 'episodes',
-                icon: Icon(
-                  Icons.filter,
-                ),
+            ),
+            const BottomNavigationBarItem(
+              label: 'episodes',
+              icon: Icon(
+                Icons.filter,
               ),
-              BottomNavigationBarItem(
-                label: 'locations',
-                icon: Icon(
-                  Icons.location_on_rounded,
-                ),
+            ),
+            const BottomNavigationBarItem(
+              label: 'locations',
+              icon: Icon(
+                Icons.location_on_rounded,
               ),
-            ],
-            currentIndex: _navBarNotifiers.index.value,
-            onTap: (index) {
-              _navBarNotifiers.index.value = index;
-              _pageController.jumpToPage(index);
-            },
-          );
-        },
-      ),
-    );
+            ),
+          ],
+          currentIndex: _screenIndex,
+          onTap: (index) {
+            setState(() {
+              _screenIndex = index;
+            });
+          },
+        ));
   }
 }
